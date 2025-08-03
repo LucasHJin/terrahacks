6d88da03510723f3a746d5183aa8fa298ddce4ac
Lucas Jin <kblazer20@gmail.com>
2025-08-03 03:27:56 -0400

    Slight mobile styling change

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import ProtectedRoute from "@/components/ProtectedRoute";
 export default function Home() {
   return (
     <ProtectedRoute>
-      <div className="min-h-screen flex items-center justify-center mobile-page-container" style={{ backgroundColor: '#f8fbfc', fontFamily: 'Inter, sans-serif' }}>
+      <div className="min-h-screen flex items-center justify-center mobile-page-container mobile-landing-container" style={{ backgroundColor: '#f8fbfc', fontFamily: 'Inter, sans-serif' }}>
         <div className="text-center">
           <h1 className="text-5xl font-medium mb-8" style={{ color: '#071012', fontWeight: 500 }}>
             Welcome to <span style={{ color: '#f0bc67' }}>BeProud</span>
